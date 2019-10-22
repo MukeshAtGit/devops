4233cc9a347d7f1dfba640a43c8f23b438c5204e
MukeshAtGit <mukeshyadav236@gmail.com>
2019-10-22 16:43:04 +0530

    add get method

--- src/main/scala/app/DevOps.scala
+++ src/main/scala/app/DevOps.scala
@@ -1,6 +1,9 @@
 package app
+
+
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
+import akka.http.scaladsl.model.HttpRequest
 import akka.stream.ActorMaterializer
 import controller.Controller
 
@@ -8,25 +11,34 @@ import scala.concurrent.{ ExecutionContextExecutor, Future }
 import scala.util.{ Failure, Success }
 
   object DevOps extends App {
-
     implicit val system: ActorSystem = ActorSystem("DataLateActorSystem")
     implicit val executionContext: ExecutionContextExecutor = system.dispatcher
     implicit val materialize: ActorMaterializer =ActorMaterializer()
     private val routes = new Controller
     private val serverBinding: Future[Http.ServerBinding] =
       Http().bindAndHandle(routes.routes,"localhost",8000 )
+    val listName=List[String]("mukesh","anshika","munna","anuj")
 
     serverBinding.onComplete {
       case Success(bound) =>
         println(
           s"Server online at http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/")
+        listName.foreach{ name =>
+          val url = s"http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/name/$name"
+          Http().singleRequest(HttpRequest(uri = url))
+          Thread.sleep(5000)
+        }
+        Http()(system)
+          .shutdownAllConnectionPools()
+        system.terminate()
       case Failure(error) =>
         Http()(system)
           .shutdownAllConnectionPools()
           .andThen {
             case _ =>
-             println("failed" + error)
+              println("failed" + error)
               system.terminate()
           }
     }
+
   }
