e84a6545512a3998e145d61ba6e77dd436512a98
MukeshAtGit <mukeshyadav236@gmail.com>
2019-10-25 02:06:53 +0530

    terminate

--- src/main/scala/app/DevOps.scala
+++ src/main/scala/app/DevOps.scala
@@ -3,7 +3,6 @@ package app
 
 import akka.actor.ActorSystem
 import akka.http.scaladsl.Http
-import akka.http.scaladsl.model.HttpRequest
 import akka.stream.ActorMaterializer
 import controller.Controller
 
@@ -17,20 +16,12 @@ import scala.util.{ Failure, Success }
     private val routes = new Controller
     private val serverBinding: Future[Http.ServerBinding] =
       Http().bindAndHandle(routes.routes,"localhost",8000 )
-    val listName=List[String]("mukesh","sachin","munna","anuj")
 
     serverBinding.onComplete {
       case Success(bound) =>
         println(
           s"Server online at http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/")
-        listName.foreach{ name =>
-          val url = s"http://${ bound.localAddress.getHostName }:${ bound.localAddress.getPort }/name/$name"
-          Http().singleRequest(HttpRequest(uri = url))
-          Thread.sleep(5000)
-        }
-        Http()(system)
-          .shutdownAllConnectionPools()
-        system.terminate()
+
       case Failure(error) =>
         Http()(system)
           .shutdownAllConnectionPools()
--- src/main/scala/controller/Controller.scala
+++ src/main/scala/controller/Controller.scala
@@ -1,7 +1,9 @@
 package controller
 
+import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.http.scaladsl.server.Route
+import app.DevOps.system
 
 class Controller {
 
@@ -10,8 +12,19 @@ class Controller {
       pathPrefix("name" / Segment) { name =>
        println("hello "+ name)
         complete(
-          "My example DevOps route"
+          s"My example DevOps route $name"
         )
       }
+    } ~ get{
+      pathPrefix("exit" ) {
+        println("bye bye")
+        Http()(system)
+          .shutdownAllConnectionPools()
+        system.terminate()
+        complete(
+          "bye bye"
+        )
+      }
+
     }
 }
